d6a6050c27656c3c0b32ced09280086659a340a4
Shunta Kimura <skimura@c2r5s2.42tokyo.jp>
2025-06-03 18:54:18 +0900

    1

---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skimura <skimura@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/04 19:26:11 by skimura           #+#    #+#             */
+/*   Updated: 2025/05/19 13:51:09 by skimura          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	if (!s)
+		return (0);
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*str;
+	size_t	i;
+	size_t	len;
+
+	if (!s)
+		return (NULL);
+	len = ft_strlen(s);
+	str = malloc(len + 1);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+int	ft_check_nl(const char *s)
+{
+	if (!s)
+		return (0);
+	while (*s)
+	{
+		if (*s == '\n')
+			return (1);
+		s++;
+	}
+	return (0);
+}
+
+char	*ft_strjoin_free(char *old, const char *buffer)
+{
+	char	*new;
+	size_t	i;
+	size_t	j;
+
+	if (!buffer)
+		return (free(old), NULL);
+	if (!old)
+		return (ft_strdup(buffer));
+	new = malloc(ft_strlen(old) + ft_strlen(buffer) + 1);
+	if (!new)
+		return (free(old), NULL);
+	i = -1;
+	while (old[++i])
+		new[i] = old[i];
+	j = 0;
+	while (buffer[j])
+		new[i++] = buffer[j++];
+	new[i] = '\0';
+	free(old);
+	return (new);
+}
+
+char	*ft_substr(const char *s, size_t start, size_t len)
+{
+	char	*sub;
+	size_t	i;
+
+	if (!s)
+		return (NULL);
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
+	if (len > ft_strlen(s + start))
+		len = ft_strlen(s + start);
+	sub = malloc(len + 1);
+	if (!sub)
+		return (NULL);
+	i = 0;
+	while (i < len && s[i + start])
+	{
+		sub[i] = s[i + start];
+		i++;
+	}
+	sub[i] = '\0';
+	return (sub);
+}
